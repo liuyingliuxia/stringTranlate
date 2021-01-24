6634cbb97141667a979af63b80cf0336406fb7fc
miracle.lin <miracle.lin@zeasn.com>
2021-01-24 14:21:05 +0800

    部分语言翻译还是不支持，新增异常的判断

--- Include/stringXML.py
+++ Include/stringXML.py
@@ -19,24 +19,35 @@ secretKey = 'rX_4tltNBEZMl5PsxW79'  # 填写你的密钥
 
 httpClient = None
 myurl = '/api/trans/vip/translate'
-languageList = ['auto', 'cht', 'zh', 'jp', 'kor', 'fra', 'spa', 'th', 'ara', 'ru', 'pt', 'de', 'it',
-                'el', 'nl', 'pl', 'bul',
-                'dan', 'fin', 'cs', 'rom', 'slo', 'swe', 'hu', 'cht', 'vie']
+
+# 把需要翻译的语言全部写在languageList里，批量翻译
+# languageList = ['auto', 'cht', 'zh', 'jp', 'kor', 'fra', 'spa', 'th', 'ara', 'ru', 'pt', 'de', 'it',
+#                 'el', 'nl', 'pl', 'bul',
+#                 'dan', 'fin', 'cs', 'rom', 'slo', 'swe', 'hu', 'cht', 'vie']
+# languageList = ['heb', 'sec', 'mot', 'mac', 'bos', 'alb', 'moc', 'hrv', 'slo', 'bul', 'geo']
+
+# 希伯来语heb,塞尔维亚-克罗地亚语sec,黑山语mot,马其顿语mac,波斯尼亚语	bos,阿尔巴尼亚语alb,
+# 蒙古语（西里尔）	moc ,	克罗地亚语	hrv,斯洛文尼亚语	slo,保加利亚语bul,格鲁吉亚语	geo
+
+languageList = ['bul', 'slo']
+# 保加利亚语bul，斯洛文尼亚语	slo
+
 # 具体语言对照详见README.md
 
 fromLang = 'en'  # 原文语种
-toLang = languageList[5]  # 译文语种
+# toLang = languageList[5]  # 译文语种
 salt = random.randint(32768, 65536)
-speed = 0.8  # 默认值为1 请求接口的频率 最快0.8了，再快就要充钱了
+speed = 0.1  # 默认值为1 请求接口的频率 最快0.8了，再快就要充钱了
 # 使用minidom解析器打开 XML 文档
-fromFileName = xml.dom.minidom.parse("test0.xml")
+fromFileName = xml.dom.minidom.parse("strings.xml")
 collection = fromFileName.documentElement
 
+
 # 最后保存的文档
-toFileName = 'strings_' + toLang + '.xml'
+# toFileName = 'strings_' + toLanguage + '.xml'
 
 
-def autoTranslate():
+def autoTranslate(toLanguage):
     strings = collection.getElementsByTagName("string")
     nameList = []
     keyList = []
@@ -45,18 +56,18 @@ def autoTranslate():
         # print("name: %s" % myString.getAttribute("name"))
         keys = myString.childNodes[0].data
         # print("key== %s" % keys)
-        # print("translate== %s" % baiduTranslate(keys))
+        # print("translate== %s" % baiduTranslate(keys, toLanguage))
         # print("=========================================")
         nameList.append(myString.getAttribute('name'))
-        keyList.append(baiduTranslate(keys))
-    saveXML(nameList, keyList)
+        keyList.append(baiduTranslate(keys, toLanguage))
+    saveXML(nameList, keyList, toLanguage)
 
 
-def baiduTranslate(q):
+def baiduTranslate(q, toLanguage):
     sign = appid + q + str(salt) + secretKey
     sign = hashlib.md5(sign.encode()).hexdigest()
     myurls = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(
-        q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(
+        q) + '&from=' + fromLang + '&to=' + toLanguage + '&salt=' + str(
         salt) + '&sign=' + sign
     try:
         httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
@@ -89,24 +100,31 @@ def jsonToString(data):
     return result[0]['dst']
 
 
-def saveXML(nameList, keyList):
+def saveXML(nameList, keyList, toLanguage):
     doc = Document()  # 创建DOM文档对象
     resources = doc.createElement('resources')  # 创建根元素
     doc.appendChild(resources)
-    if len(nameList) == len(keyList):
-        for (name, key) in zip(nameList, keyList):
-            stringItem = doc.createElement('string')  # 创建string
-            stringItem.setAttribute('name', name)  # 把name加入
-            content = doc.createTextNode(key)  # 创建key
-            stringItem.appendChild(content)  # 把两个>...< 中的key 加入
-            resources.appendChild(stringItem)  # 最后把string加到resources中
-
-    f = open(toFileName, 'w', encoding='utf-8')
-    # f.write(doc.toprettyxml(indent = ' ', newl = '\n', encoding = 'utf-8'))
-    doc.writexml(f, indent=' ', newl='\n', addindent='\t', encoding='utf-8')
-    f.close()
-    print('保存文件%s成功！' % toFileName)
+    # 最后保存的文档
+    toFileName = 'strings_' + toLanguage + '.xml'
+    try:
+        if len(nameList) == len(keyList):
+            for (name, key) in zip(nameList, keyList):
+                stringItem = doc.createElement('string')  # 创建string
+                stringItem.setAttribute('name', name)  # 把name加入
+                content = doc.createTextNode(key)  # 创建key
+                stringItem.appendChild(content)  # 把两个>...< 中的key 加入
+                resources.appendChild(stringItem)  # 最后把string加到resources中
+    except TypeError as err:
+        print('所有值都为空！翻译失败', err)
+    finally:
+        f = open(toFileName, 'w', encoding='utf-8')
+        # f.write(doc.toprettyxml(indent = ' ', newl = '\n', encoding = 'utf-8'))
+        doc.writexml(f, indent=' ', newl='\n', addindent='\t', encoding='utf-8')
+        f.close()
+        print('保存文件%s成功！' % toFileName)
 
 
 if __name__ == '__main__':
-    autoTranslate()
+    for toLan in languageList:
+        autoTranslate(toLan)
+    # autoTranslate('jp')
